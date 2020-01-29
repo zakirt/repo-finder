d89633ce3057c161d6a2716a4c904a9b92432a68
zakirt <zakir.tariverdiev@yahoo.com>
2020-01-28 21:42:23 -0500

    Improve error messages

--- src/validations/repoSearchValidationRules.ts
+++ src/validations/repoSearchValidationRules.ts
@@ -3,13 +3,13 @@ import { string, object } from 'yup';
 export const repoSearchValidationRules = object().shape({
     query: string()
         .required('Query is required')
-        .matches(/^[-_a-z0-9\s]+$/i, {
+        .matches(/^[a-z0-9\s]+$/i, {
             message: 'Alpha-numeric characters and spaces only',
             excludeEmptyString: true
         }),
     stars: string()
         .matches(/^(([0-9]+)|([0-9]+[.]{2}[0-9]+)|([><]{1}=?[0-9]+))$/, {
-            message: 'Invalid value for stars',
+            message: 'Format must be N, N..N, >N, <N, >=N, <=N',
             excludeEmptyString: true
         }),
 });
